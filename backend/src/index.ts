da7a43270e1d0deed2e5317e23aa06509d07b6c0
xcon <carlconrad98071234@gmail.com>
2025-12-03 14:12:24 +0800

    minor changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import { Server } from "socket.io";
+import http from "http";
 import { pool, connectDB } from "./config/db";
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -37,6 +49,36 @@ app.get("/users", async (_req: Request, res: Response) => {
   }
 });
 
+// ----------------------------
+//    Socket.io Integration
+// ----------------------------
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  // Handle Messages
+  socket.on("chat message", (msg) => {
+    try {
+      console.log("Message received:", msg);
+      io.emit("chat message", msg);
+    } catch (err) {
+      console.error("Error handling chat message:", err);
+    }
+  });
+
+  // Handle Disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on("error", (err) => {
+    console.error("Socket Error", err);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 async function startServer() {
   try {
     await connectDB();
