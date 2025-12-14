6c36948e7ebc67ce1e17dd6b32cacfa5141ceb30
cloud <cloudneo0719@gmail.com>
2025-12-14 10:24:36 +0800

    Update button shadcn used

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "@/components/ui/Button";
+import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
     <main className="p-8">
-      <Button variant="ghost" className="ml-2">
-        Outline
-      </Button>
+      <Button>Hello</Button>
     </main>
   );
 }
