5ae639f7a07c9e2c8e002370bafe4323d2ff64c7
cloud <cloudneo0719@gmail.com>
2025-12-22 22:54:15 +0800

    Added dialog component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/Button";
 import { Card, CardHeader, CardTitle, CardDescription, CardAction } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex p-8 justify-center mt-2">
+      {/* This is just a sample it can be replaced */}
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle>Welcome</CardTitle>
