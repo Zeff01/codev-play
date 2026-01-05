5a1b7db8ff7cdc39c30d5fb115c0fa783a74894e
cloud <cloudneo0719@gmail.com>
2026-01-05 17:09:31 +0800

    Add layout and other features

---
+++ frontend/src/app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import { Sidebar } from "@/components/sidebar";
+import { Header } from "@/components/header";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4">{children}</main>
+      </div>
+    </div>
+  );
+}
