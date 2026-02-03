d48852f00e1157e767c1f19f329f7ca6ebbd4341
cloud <cloudneo0719@gmail.com>
2026-02-03 08:00:44 +0800

    Improve fetch hook

--- frontend/src/hooks/useApiFetch.ts
+++ frontend/src/hooks/useApiFetch.ts
@@ -3,23 +3,26 @@
 import { apiFetch } from "@/lib/utils";
 import { useState } from "react";
 
-export function useApiFetch(endpoint: string) {
+export function useApiFetch() {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchedData = async () => {
+  const request = async (endpoint: string, options?: RequestInit) => {
     setLoading(true);
     setError(null);
+
     try {
-      const res = await apiFetch(endpoint);
+      const res = await apiFetch(endpoint, options);
       setData(res);
+      return res;
     } catch (err: any) {
       setError(err.message || "Error in fetching data");
+      throw err;
     } finally {
       setLoading(false);
     }
   };
 
-  return { data, loading, error, fetchedData };
+  return { data, loading, error, request };
 }
